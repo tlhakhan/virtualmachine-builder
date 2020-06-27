243755081d5d9f8cae260052bca005745dcce1af
Tenzin Lhakhang <tenzin.lhakhang@gmail.com>
2020-06-27 00:11:51 -0400

    create packer workspace

---
+++ workspace/start.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# the var file named after the vm
+VM_NAME=$1
+
+if [[ -z $VM_NAME ]]
+then
+  echo "Provide a virtual machine name."
+  exit 1
+fi
+
+packer validate --var-file /packer/var-files/$VM_NAME.json /packer/templates/vm.json
+
+packer build --var-file /packer/var-files/$VM_NAME.json /packer/templates/vm.json
