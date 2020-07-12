a0a722d7c0e803dc0e786ea14f63850b95dc967f
Tenzin Lhakhang <tenzin.lhakhang@gmail.com>
2020-07-12 19:01:51 -0400

    updates to start and vm.json

--- workspace/start.sh
+++ workspace/start.sh
@@ -10,6 +10,7 @@ then
   exit 1
 fi
 
+echo Validating virtual machine configuration for $VM_NAME.
 packer validate --var-file /packer/var-files/$VM_NAME.json /packer/templates/vm.json
-
+echo Performing virtual machine build for $VM_NAME.
 packer build --var-file /packer/var-files/$VM_NAME.json /packer/templates/vm.json
