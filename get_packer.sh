308e2b32d1d48d1a49d04653429a902f589a9aee
Tenzin Lhakhang <tenzin.lhakhang@gmail.com>
2021-02-01 02:53:38 +0000

    better use of lsb_release

--- get_packer.sh
+++ get_packer.sh
@@ -2,12 +2,12 @@
 
 # ubuntu
 get_packer_ubuntu() {
-  curl -fsSL https://apt.releases.hashicorp.com/gpg | apt-key add -
-  apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
-  apt-get update && sudo apt-get install packer
+  curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
+  sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
+  sudo apt-get update && sudo apt-get install packer
 }
 
-if [[ $(lsb_release -i | awk -F ':\t' '{print $2}') == "Ubuntu" ]]
+if [[ $(lsb_release -si) == "Ubuntu" ]]
 then
   get_packer_ubuntu
 fi
