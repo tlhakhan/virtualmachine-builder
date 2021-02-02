f5a98a0b4a4fa2a00bc9562ca35172f832eab3cf
Tenzin Lhakhang <tenzin.lhakhang@gmail.com>
2021-02-02 15:22:14 +0000

    update get_packer script

--- get_packer.sh
+++ get_packer.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
+figlet get packer
+
 # ubuntu
 get_packer_ubuntu() {
+  echo Detected $(lsb_release -sd)
+  echo Getting gpg key
   curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
+  echo "Adding apt.releases.hashicorp.com repository"
   sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
+  echo "Installing packer"
   sudo apt-get update && sudo apt-get install packer
 }
 
