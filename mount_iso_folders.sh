425b48ba5ca18b8d33b7cd2b67f720d6e7c2a405
Tenzin Lhakhang <tenzin.lhakhang@gmail.com>
2021-02-21 03:39:56 +0000

    ignore mount packer_cache iso files

--- mount_iso_folders.sh
+++ mount_iso_folders.sh
@@ -5,7 +5,14 @@ BASEDIR=$(dirname $BASH_SOURCE)
 # find all iso files
 for ISO_FILE in $(find $BASEDIR -type f -name "*.iso")
 do
-  LOCAL_ISO_DIR="$(dirname $ISO_FILE)/iso"
+
+  LOCAL_DIR="$(dirname $ISO_FILE)"
+  if [[ $LOCAL_DIR =~ packer_cache$ ]]
+  then
+    continue
+  fi
+
+  LOCAL_ISO_DIR="$LOCAL_DIR/iso"
   # mount the iso file to a local folder called iso
   test -d $LOCAL_ISO_DIR || mkdir $LOCAL_ISO_DIR
   mount -v -o ro $ISO_FILE $LOCAL_ISO_DIR 
