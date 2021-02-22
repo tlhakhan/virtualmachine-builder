7466693fdfaaa02dc85d733d9fca85d255a20ba1
Tenzin Lhakhang <tenzin.lhakhang@gmail.com>
2021-02-22 00:19:06 +0000

    enhance container build logic and speed up builder.sh

--- builder.sh
+++ builder.sh
@@ -5,6 +5,6 @@
 # create a volume to store the packer_env vars
 # run the container image and pass through arguments array to the running container
 #
-docker build . -t packer-esxi:latest
-docker volume create packer_env
+docker image inspect packer-esxi:latest &> /dev/null || docker build -t packer-esxi:latest .
+docker volume create packer_env &> /dev/null
 docker run --network=host -it -v packer_env:/packer_env --rm packer-esxi:latest $@
