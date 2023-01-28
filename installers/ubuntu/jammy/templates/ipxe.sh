4f976134bf0129b2144abc1e9d3623def2cd9c6e
Tenzin Lhakhang <2402007+tlhakhan@users.noreply.github.com>
2023-01-28 11:38:25 -0500

    Update ipxe.sh

--- installers/ubuntu/jammy/templates/ipxe.sh
+++ installers/ubuntu/jammy/templates/ipxe.sh
@@ -20,3 +20,8 @@ set iso_url http://{{.HTTPAddress}}/files/ubuntu-22.04.1-live-server-amd64.iso
 kernel ${vmlinuz_url} autoinstall url=${iso_url} net.ifnames=0 biosdevname=0 ip=::::{{ index .PackerVars "vm_name" }}::dhcp ds=nocloud-net;s=${seed_url}
 initrd ${initrd_url}
 boot
+
+# Notes on what ip= kernel boot parameter means
+# https://www.kernel.org/doc/Documentation/filesystems/nfs/nfsroot.txt
+# The purpose of ip= in this script is to configure the hostname of the initial ubuntu installer environment, so that doesn't use the default ubuntu-server hostname.
+# This causes confusion at the DHCP level if you do many concurrent/parallel builds.
