c1cb23c54f0da7909a674724be1dc6068746eb2e
Tenzin Lhakhang <2402007+tlhakhan@users.noreply.github.com>
2023-03-05 18:09:21 -0500

    Add post-install script for customizations

---
+++ installers/debian/bullseye/templates/post-install.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# No password needed for privilege escalation
+cat <<EOF > /etc/sudoers.d/{{ index .PackerVars "vm_username" }}
+{{ index .PackerVars "vm_username" }} ALL=(ALL) NOPASSWD:ALL
+EOF
+
+# Add authorized_keys for root
+test -d /root/.ssh || mkdir /root/.ssh
+cat <<EOF > /root/.ssh/authorized_keys
+{{ index .PackerVars "vm_ssh_public_key" }}
+EOF
+chmod 0700 /root/.ssh
+chmod 0600 /root/.ssh/authorized_keys
+
+# Add authorized_keys for {{ index .PackerVars "vm_username" }}
+test -d /home/{{ index .PackerVars "vm_username" }}/.ssh || mkdir /home/{{ index .PackerVars "vm_username" }}/.ssh
+cat <<EOF > /home/{{ index .PackerVars "vm_username" }}/.ssh/authorized_keys
+{{ index .PackerVars "vm_ssh_public_key" }}
+EOF
+chmod 0700 /home/{{ index .PackerVars "vm_username" }}/.ssh
+chmod 0600 /home/{{ index .PackerVars "vm_username" }}/.ssh/authorized_keys
+chown --recursive {{ index .PackerVars "vm_username" }}:{{ index .PackerVars "vm_username" }} /home/{{ index .PackerVars "vm_username" }}/.ssh
+
+# Install ansible
+apt-get update
+apt-get install -y python3-pip
+pip3 install --upgrade pip
+pip install --upgrade ansible
+ansible-galaxy collection install --timeout 180 community.general
