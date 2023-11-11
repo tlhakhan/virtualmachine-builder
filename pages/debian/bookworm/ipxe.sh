a7dea49527239452b2c6b0c0d45437a7897a17d4
Tenzin Lhakhang <2402007+tlhakhan@users.noreply.github.com>
2023-11-11 16:12:34 -0500

    Make scripts more general

--- pages/debian/bookworm/ipxe.sh
+++ pages/debian/bookworm/ipxe.sh
@@ -2,10 +2,10 @@
 #
 # Debian Installer
 #
-set preseed_url http://{{.HTTPAddress}}/templates/preseed.cfg 
+set preseed_url https://tlhakhan.github.io/vmware-builder/debian/bookworm/preseed.cfg
 set linux_url http://deb.debian.org/debian/dists/bookworm/main/installer-amd64/current/images/netboot/debian-installer/amd64/linux
 set initrd_url http://deb.debian.org/debian/dists/bookworm/main/installer-amd64/current/images/netboot/debian-installer/amd64/initrd.gz
 
-kernel ${linux_url} auto=true priority=critical interface=auto ipv6.disable=1 preseed/url=${preseed_url} DEBIAN_FRONTEND=text net.ifnames=0
+kernel ${linux_url} auto=true priority=critical interface=auto preseed/url=${preseed_url} DEBIAN_FRONTEND=text
 initrd ${initrd_url}
 boot
