e13c0532bd9863f1e2dfd702d342a68edd02b074
Tenzin Lhakhang <2402007+tlhakhan@users.noreply.github.com>
2023-11-11 17:11:17 -0500

    Add overrides.pkrvars.hcl guard

--- builder.sh
+++ builder.sh
@@ -3,11 +3,15 @@
 # Prompt the user for the name of the virtual machine
 read -p "Name of virtual machine? " vm_name
 
-# Display the entered virtual machine name
-echo "You entered: $vm_name"
-
 # Export required variables
 export PKR_VAR_vm_name="$vm_name"
+
+if [[ ! -e "overrides.pkrvars.hcl" ]]
+then
+  echo
+  echo Missing overrides.pkrvars.hcl file.  See README.md for more details.
+  exit 1
+fi 
 
 # Run packer
 packer validate -var-file overrides.pkrvars.hcl packer_template.pkr.hcl
