d8dad3706c4c0768fd4cd292b5a7bed65962d9ee
Tenzin Lhakhang <2402007+tlhakhan@users.noreply.github.com>
2023-11-11 23:22:26 -0500

    Run as packer

--- docs/debian/bookworm/sysprep.sh
+++ docs/debian/bookworm/sysprep.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Create SSH keys and authorized_keys file
-ssh-keygen -N "" -q -f /home/packer/.ssh/id_rsa
-touch /home/packer/.ssh/authorized_keys
-chmod 0600 /home/packer/.ssh/authorized_keys
+# Create SSH keys and authorized_keys file for packer user
+su packer -l -c "ssh-keygen -N '' -q -f ~/.ssh/id_rsa"
+su packer -l -c "touch ~/.ssh/authorized_keys"
+su packer -l -c "chmod 0600 ~/.ssh/authorized_keys"
