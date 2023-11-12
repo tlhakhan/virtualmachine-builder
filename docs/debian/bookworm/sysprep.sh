aa77681291442900c9c6cdd03c56be99bb00d9ae
Tenzin Lhakhang <2402007+tlhakhan@users.noreply.github.com>
2023-11-12 09:27:44 -0500

    Remove ssh-keygen command

--- docs/debian/bookworm/sysprep.sh
+++ docs/debian/bookworm/sysprep.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 # Create SSH keys and authorized_keys file for packer user
-su packer -l -c "ssh-keygen -N '' -q -f ~/.ssh/id_rsa"
+su packer -l -c "mkdir -m 0700 ~/.ssh"
 su packer -l -c "touch ~/.ssh/authorized_keys"
 su packer -l -c "chmod 0600 ~/.ssh/authorized_keys"
