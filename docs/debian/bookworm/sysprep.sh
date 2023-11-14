8164c80eb206872749f37089fcf96d122cf3c57a
Tenzin Lhakhang <2402007+tlhakhan@users.noreply.github.com>
2023-11-13 22:22:38 -0500

    Add SSH CA placeholder config

--- docs/debian/bookworm/sysprep.sh
+++ docs/debian/bookworm/sysprep.sh
@@ -4,3 +4,17 @@
 su packer -l -c "mkdir -m 0700 ~/.ssh"
 su packer -l -c "touch ~/.ssh/authorized_keys"
 su packer -l -c "chmod 0600 ~/.ssh/authorized_keys"
+
+# Setup SSH CA
+mkdir /etc/ssh/auth_principals
+touch /etc/ssh/trusted_ssh_ca.pub
+
+cat <<EOF >> /etc/ssh/sshd_config
+
+# SSH CA configuration
+AuthorizedPrincipalsFile /etc/ssh/auth_principals/%u
+TrustedUserCAKeys /etc/ssh/trusted_ssh_ca.pub
+EOF
+
+# Setup auth principals file for packer user
+echo packer > /etc/ssh/auth_principals/packer
